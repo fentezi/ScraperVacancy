f8b6ba0453adf481f8eacd6a75de239863185b5b
fentezi <serouhn@gmail.com>
2024-02-15 22:04:07 +0200

    Add goroutines

--- internal/handler/handler_jobs.go
+++ internal/handler/handler_jobs.go
@@ -3,13 +3,24 @@ package handler
 import (
 	"github.com/fentezi/scraper/pkg"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"time"
 )
 
 func (h *Handler) Jobs(c *gin.Context) {
-	infoDjinni := pkg.ParseDjinni()
-	infoDou := pkg.ParserDou()
-
+	start := time.Now()
+	chDjinni := make(chan []pkg.InfoDjinni)
+	chDou := make(chan []pkg.InfoDou)
+	go func() {
+		chDjinni <- pkg.ParseDjinni()
+	}()
+	go func() {
+		chDou <- pkg.ParserDou()
+	}()
+	infoDjinni := <-chDjinni
+	infoDou := <-chDou
+	log.Println("Time:", time.Since(start))
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"infoDjinni": infoDjinni,
 		"infoDou":    infoDou,
